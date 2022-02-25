f742ee0de38050f81e902d46c78d1a48a01d864d
pirate-kor <50766800+pirate-kor@users.noreply.github.com>
2022-02-25 21:18:05 +0900

    multi module 내용 수정

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("org.springframework.boot") version "2.6.4" apply false
+    id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
+
+    kotlin("jvm") version "1.6.10" apply false
+    kotlin("plugin.spring") version "1.6.10" apply false
+    kotlin("plugin.jpa") version "1.6.10" apply false
+}
+
+allprojects {
+    group = "com.pirate.practice"
+    version = "1.0.0"
+
+    tasks.withType<JavaCompile> {
+        sourceCompatibility = "11"
+        targetCompatibility = "11"
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "11"
+            incremental = false
+        }
+    }
+}
+
+subprojects {
+    repositories {
+        mavenCentral()
+    }
+
+    apply {
+        plugin("io.spring.dependency-management")
+    }
+}
+
