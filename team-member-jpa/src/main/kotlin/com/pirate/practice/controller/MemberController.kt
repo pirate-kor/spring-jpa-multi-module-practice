fef174abc01a81b317462976548fc4e640c40855
pirate-kor <50766800+pirate-kor@users.noreply.github.com>
2022-03-02 21:20:46 +0900

    페이징 추가

--- team-member-jpa/src/main/kotlin/com/pirate/practice/controller/MemberController.kt
+++ team-member-jpa/src/main/kotlin/com/pirate/practice/controller/MemberController.kt
@@ -1,7 +1,11 @@
 package com.pirate.practice.controller
 
+import com.pirate.practice.dto.MemberDto
 import com.pirate.practice.entity.Member
 import com.pirate.practice.repository.MemberRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.web.PageableDefault
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
@@ -25,9 +29,17 @@ class MemberController(
         return member.username
     }
 
+    @GetMapping("/members")
+    fun list(@PageableDefault(size = 5) pageable: Pageable): Page<MemberDto> {
+        return memberRepository.findAll(pageable)
+            .map { MemberDto(it) }
+    }
+
     @PostConstruct
     fun init() {
-        memberRepository.save(Member("member1"))
+        for (i in 0 .. 100) {
+            memberRepository.save(Member("member$i", i))
+        }
     }
 
 }
--- team-member-jpa/src/main/kotlin/com/pirate/practice/dto/MemberDto.kt
+++ team-member-jpa/src/main/kotlin/com/pirate/practice/dto/MemberDto.kt
@@ -1,7 +1,11 @@
 package com.pirate.practice.dto
 
+import com.pirate.practice.entity.Member
+
 data class MemberDto(
     val id: Long,
     val username: String,
     val teamName: String
-)
+) {
+    constructor(member: Member): this(member.id ?: 0, member.username, member.team?.name ?: "")
+}
